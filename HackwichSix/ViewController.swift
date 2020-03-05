086a13c6b0670f16264023efb0bd2859c0e3ed37
eonderko <eonderko@hawaii.edu>
2020-03-05 10:40:03 -1000

    completed problem sets

--- HackwichSix/ViewController.swift
+++ HackwichSix/ViewController.swift
@@ -23,6 +23,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
         let text = countries[indexPath.row]
         cell.textLabel?.text = text
+        cell.detailTextLabel?.text = friendsHomeArray[indexPath.row]
         return cell
         
     }
@@ -31,7 +32,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     var countries = ["Finland", "Japan", "Germany"]
     
-
+    var friendsHomeArray = ["Helsinki", "Tokyo", "Cologne"]
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
